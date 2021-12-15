34a749b869d2a5c2a9ad98dff82824574d831ca0
Tim Morello <tdmorello@gmail.com>
2021-12-14 22:55:43 -0500

    Add test for tiler widget

--- src/napari_tiler/_tests/test_dock_widget.py
+++ src/napari_tiler/_tests/test_dock_widget.py
@@ -18,6 +18,7 @@ def test_something_with_viewer(widget_name, make_napari_viewer, napari_plugin_ma
     assert len(viewer.window._dock_widgets) == num_dw + 1
 
 
+# TODO remove scikit-image and pooch dependencies from testing
 @pytest.mark.parametrize("image_name", ["cells3d", "astronaut"])
 def test_tiler_widget(image_name, make_napari_viewer, napari_plugin_manager):
     napari_plugin_manager.register(napari_tiler, name=MY_PLUGIN_NAME)
@@ -31,13 +32,16 @@ def test_tiler_widget(image_name, make_napari_viewer, napari_plugin_manager):
     assert len(viewer.layers) == num_layers + 1
 
 
-# def test_generate_preview(make_napari_viewer, napari_plugin_manager):
-#     napari_plugin_manager.register(napari_tiler, name=MY_PLUGIN_NAME)
-#     viewer = make_napari_viewer()
-#     widget = viewer.window.add_plugin_dock_widget(
-#         plugin_name=MY_PLUGIN_NAME, widget_name="Tiler Widget"
-#     )[-1]
-#     image = viewer.open_sample("scikit-image", "astronaut")
+def test_generate_preview(make_napari_viewer, napari_plugin_manager):
+    napari_plugin_manager.register(napari_tiler, name=MY_PLUGIN_NAME)
+    viewer = make_napari_viewer()
+    _, widget = viewer.window.add_plugin_dock_widget(
+        plugin_name=MY_PLUGIN_NAME, widget_name="Tiler Widget"
+    )
+    image = viewer.open_sample("scikit-image", "astronaut")
+    num_layers = len(viewer.layers)
+    widget.preview_chkb.setChecked(True)
+    assert len(viewer.layers) == num_layers + 1
 
 
 # def test_tiler_widget_rgb(make_napari_viewer, napari_plugin_manager):
