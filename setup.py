38f1b7f75240a412c1e948157773aaf74434f802
Tim Morello <tdmorello@gmail.com>
2021-12-16 17:26:21 -0500

    Switch to git tag versioning

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup()
+# https://github.com/pypa/setuptools_scm
+use_scm = {"write_to": "src/napari_tiler/_version.py"}
+setup(use_scm_version=use_scm)
--- src/napari_tiler/__init__.py
+++ src/napari_tiler/__init__.py
@@ -1,3 +1,6 @@
-__version__ = "0.0.2"
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
 
 from ._dock_widget import napari_experimental_provide_dock_widget  # noqa: F401
