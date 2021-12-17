704bf021f6c14b8f49117567e66f03d1d4a87670
Tim Morello <tdmorello@gmail.com>
2021-12-17 18:05:46 -0500

    Fix nox test

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,11 @@ from nox.sessions import Session
 @nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def tests(session: Session) -> None:
     """Run tests."""
-    session.run("poetry", "install", "--no-dev", external=True)
+    # session.run("poetry", "install", "--no-dev", external=True)
+    session.run("poetry", "install", external=True)
+    session.install(
+        "pytest",
+    )
     session.run("pytest")
 
 
