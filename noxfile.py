d08b8237f90aa850875d4a5a9c30541fe7fb4aae
Tim Morello <tdmorello@gmail.com>
2021-12-18 18:29:36 -0500

    Update env

--- noxfile.py
+++ noxfile.py
@@ -6,11 +6,8 @@ from nox.sessions import Session
 @nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def tests(session: Session) -> None:
     """Run tests."""
-    # session.run("poetry", "install", "--no-dev", external=True)
-    session.run("poetry", "install", external=True)
-    session.install(
-        "pytest",
-    )
+    session.run("poetry", "shell")
+    session.run("poetry", "install")
     session.run("pytest")
 
 
