112cc36b5f31348f96fbbb0f60e85114cbd07c4c
Tim Morello <tdmorello@gmail.com>
2021-12-20 11:13:32 -0500

    Update noxfile

--- noxfile.py
+++ noxfile.py
@@ -11,6 +11,22 @@ python_versions = ["3.7", "3.8", "3.9", "3.10"]
 @nox.session(python=python_versions)
 def tests(session: Session) -> None:
     """Run the test suite."""
+    _install_via_pip(session)
+
+    session.install("napari[all]", "pytest", "pytest-cov", "pytest-qt")
+    if sys.platform == "linux":
+        session.install("pytest-xvfb")
+
+    session.run("pytest")
+
+
+@nox.session
+def lint(session: Session) -> None:
+    """Run linting."""
+    pass
+
+
+def _install_via_pip(session: Session) -> None:
     with tempfile.NamedTemporaryFile() as requirements:
         if sys.platform == "win32":
             requirements_path = "requirements.txt"
@@ -25,15 +41,3 @@ def tests(session: Session) -> None:
             external=True,
         )
         session.install("-r", requirements_path)
-
-    session.install("napari[all]", "pytest", "pytest-cov", "pytest-qt")
-    if sys.platform == "linux":
-        session.install("pytest-xvfb")
-
-    session.run("pytest")
-
-
-@nox.session
-def lint(session: Session) -> None:
-    """Run linting."""
-    pass
