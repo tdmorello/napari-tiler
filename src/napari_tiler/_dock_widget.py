937a7792dc9d97991ad5422ad3abc747c659a646
Tim Morello <tdmorello@gmail.com>
2021-12-21 15:40:59 -0500

    Update formatting

--- src/napari_tiler/_dock_widget.py
+++ src/napari_tiler/_dock_widget.py
@@ -1,6 +1,4 @@
-"""
-This provides the widgets to make or merge tiles
-"""
+"""This provides the widgets to make or merge tiles."""
 from napari_plugin_engine import napari_hook_implementation
 
 from .merger_widget import MergerWidget
@@ -8,5 +6,5 @@ from .tiler_widget import TilerWidget
 
 
 @napari_hook_implementation
-def napari_experimental_provide_dock_widget():
+def napari_experimental_provide_dock_widget() -> list:  # noqa: D103
     return [TilerWidget, MergerWidget]
